35f11b0ca92e88460db2e559e6eba041cecdd7e5
Nils <nils@vaede.com>
2022-06-21 17:06:59 +0200

    add single scraping test

--- src/components/ScrapingElement.vue
+++ src/components/ScrapingElement.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="scraping-element" :class="status">
+  <div class="scraping-element" :class="scrapingStatus" @click="doSpecificScraping">
     <div class="scraping-element-title">
       <h3>{{ websiteName }}</h3>
     </div>
     <div class="scraping-element-icon-wrapper">
-      <i class="fa-solid fa-clock scraping-element-icon" v-if="status === 'pending'"></i>
-      <i class="fa-solid fa-arrows-rotate scraping-element-icon" v-else-if="status === 'running'"></i>
-      <i class="fa-solid fa-circle-check scraping-element-icon" v-else-if="status === 'success'"></i>
-      <i class="fa-solid fa-circle-xmark scraping-element-icon" v-else-if="status === 'error'"></i>
+      <i class="fa-solid fa-clock scraping-element-icon" v-if="scrapingStatus === 'pending'"></i>
+      <i class="fa-solid fa-arrows-rotate scraping-element-icon" v-else-if="scrapingStatus === 'running'"></i>
+      <i class="fa-solid fa-circle-check scraping-element-icon" v-else-if="scrapingStatus === 'success'"></i>
+      <i class="fa-solid fa-circle-xmark scraping-element-icon" v-else-if="scrapingStatus === 'error'"></i>
     </div>
   </div>
 </template>
@@ -22,6 +22,68 @@ export default {
       default: "pending"
     }
   },
+  data() {
+    return {
+      scrapingStatus: this.status
+    }
+  },
+  watch: {
+    status(val) {
+      this.scrapingStatus = val
+    }
+  },
+  methods: {
+    async doSpecificScraping() {
+      this.scrapingStatus = "running";
+      const json_data = {
+        websiteToTest: this.websiteName
+      };
+
+      const response = await fetch('https://ow.services.eemi.tech/api/v1/web/nils.vaede/default/scraping.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(json_data)
+      });
+
+      const result = await response.json();
+      switch (this.websiteName) {
+        case "Indeed" :
+          // eslint-disable-next-line no-prototype-builtins
+          if (result.indeed.hasOwnProperty("error")) {
+            this.scrapingStatus = "error";
+          } else {
+            this.scrapingStatus = "success";
+          }
+          break;
+        case "Hello Work" :
+          // eslint-disable-next-line no-prototype-builtins
+          if (result.helloWork.hasOwnProperty("error")) {
+            this.scrapingStatus = "error";
+          } else {
+            this.scrapingStatus = "success";
+          }
+          break;
+        case "LinkedIn" :
+          // eslint-disable-next-line no-prototype-builtins
+          if (result.linkedIn.hasOwnProperty("error")) {
+            this.scrapingStatus = "error";
+          } else {
+            this.scrapingStatus = "success";
+          }
+          break;
+        case "Pôle Emploi" :
+          // eslint-disable-next-line no-prototype-builtins
+          if (result.poleEmploi.hasOwnProperty("error")) {
+            this.scrapingStatus = "error";
+          } else {
+            this.scrapingStatus = "success";
+          }
+          break;
+      }
+    }
+  }
 }
 </script>
 
@@ -34,6 +96,7 @@ export default {
   border-radius: 7px;
   margin-top: 32px;
   padding: 10px 50px;
+  cursor: pointer;
 }
 
 .scraping-element-icon-wrapper {
--- src/components/ScrapingWrapper.vue
+++ src/components/ScrapingWrapper.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="scrapping-wrapper">
-    <ScrapingElement website-name="Indeed" :status="scrapingStatus"></ScrapingElement>
-    <ScrapingElement website-name="Hello Work" :status="scrapingStatus"></ScrapingElement>
-    <ScrapingElement website-name="LinkedIn" :status="scrapingStatus"></ScrapingElement>
-    <ScrapingElement website-name="Pôle Emploi" :status="scrapingStatus"></ScrapingElement>
+    <ScrapingElement website-name="Indeed" :status="indeedStatus"></ScrapingElement>
+    <ScrapingElement website-name="Hello Work" :status="helloWorkStatus"></ScrapingElement>
+    <ScrapingElement website-name="LinkedIn" :status="linkedInStatus"></ScrapingElement>
+    <ScrapingElement website-name="Pôle Emploi" :status="poleEmploiStatus"></ScrapingElement>
     <button id="run-scraping-test" @click="callApi" v-if="scrapingStatus !== 'running'">Run scraping test</button>
     <img id="loading-img" :src="`${publicPath}loading.gif`" v-else>
   </div>
@@ -17,22 +17,49 @@ export default {
   components: {ScrapingElement},
   data() {
     return {
-      scrapingStatus: "pending",
-      indeedData: null,
-      helloWorkData: null,
-      linkedInData: null,
-      poleEmploiData: null,
+      scrapingStatus: 'pending',
+      indeedStatus: 'pending',
+      helloWorkStatus: 'pending',
+      linkedInStatus: 'pending',
+      poleEmploiStatus: 'pending',
       publicPath: process.env.BASE_URL
     }
   },
   methods: {
     async callApi() {
       this.scrapingStatus = 'running';
+      this.indeedStatus = 'running';
+      this.helloWorkStatus = 'running';
+      this.linkedInStatus = 'running';
+      this.poleEmploiStatus = 'running';
       let response = await fetch("https://ow.services.eemi.tech/api/v1/web/nils.vaede/default/scraping.json");
       if (response.ok) {
         const json = await response.json();
-        this.scrapingStatus = "success";
-        this.indeedData = json.indeed;
+        // eslint-disable-next-line no-prototype-builtins
+        if (json.helloWork.hasOwnProperty("error")) {
+          this.helloWorkStatus = 'error'
+        } else {
+          this.helloWorkStatus = 'success'
+        }
+        // eslint-disable-next-line no-prototype-builtins
+        if (json.indeed.hasOwnProperty("error")) {
+          this.linkedInStatus = 'error'
+        } else {
+          this.indeedStatus = 'success'
+        }
+        // eslint-disable-next-line no-prototype-builtins
+        if (json.linkedIn.hasOwnProperty("error")) {
+          this.linkedInStatus = 'error'
+        } else {
+          this.linkedInStatus = 'success'
+        }
+        // eslint-disable-next-line no-prototype-builtins
+        if (json.poleEmploi.hasOwnProperty("error")) {
+          this.poleEmploiStatus = 'error'
+        } else {
+          this.poleEmploiStatus = 'success'
+        }
+        this.scrapingStatus = 'pending';
       } else {
         alert("HTTP-Error: " + response.status);
       }
